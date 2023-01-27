93dfeed7fbb9b5a5741195137a551dd27285065a
odajoh <oda.johannessen@bouvet.no>
2023-01-27 12:23:21 +0100

    Add decryption service

---
+++ Bastion/Core/Domain/Decryption/Services/DecryptionService.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography;
+
+namespace Bastion.Core.Domain.Decryption.Services;
+
+public class DecryptionService : IDecryptionService
+{
+    public async Task<string> DecryptSecret(byte[] ciphertext) // TODO: Does this need to be a task?
+    {
+        // TODO: Should we implement some logging here? Security risks? 
+        // TODO: Where to store logs? Blob? 
+        // TODO: Which Aes class to use? Gcm? Cng? Document choice
+        using (Aes aes = Aes.Create())
+        {
+            string decryptedData = DecryptStringFromBytes(ciphertext, aes.Key, aes.IV);
+            return decryptedData; // TODO: Should return the secret
+
+        }
+    }
+
+    private static string DecryptStringFromBytes(byte[] ciphertext, byte[] Key, byte[] IV)
+    {
+        // Check validity of input
+        if (ciphertext == null || ciphertext.Length <= 0) 
+        {
+            throw new ArgumentNullException(nameof(ciphertext));       
+        }
+        else if (Key == null || Key.Length <= 0) 
+        {
+            throw new ArgumentNullException(nameof(Key));
+        }
+        else if (IV == null || IV.Length <= 0) 
+        {
+            throw new ArgumentNullException(nameof(IV));
+        }
+
+        string plaintext = "";
+
+        try
+        {
+            using(Aes aes = Aes.Create())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream(ciphertext))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            plaintext = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex) 
+        {
+            // TODO: Logging?
+            throw new Exception(ex.ToString());
+        }
+
+        return plaintext;
+    }
+}
---
+++ Bastion/Core/Domain/Decryption/Services/IDecryptionService.cs
@@ -0,0 +1,12 @@
+﻿namespace Bastion.Core.Domain.Decryption.Services;
+
+public interface IDecryptionService
+{
+    // TODO: Encrypt secret
+    // TODO: Store in Azure storage
+    // TODO: Input? DTO object? userInput object? 
+    // TODO: Return Bool? 
+    // TODO: Should it be a task? 
+
+    Task<string> DecryptSecret(byte[] ciphertext);
+}
