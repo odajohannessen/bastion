862c7aa318d4feed768cadc6d53bbd0bfabcc84a
odajoh <oda.johannessen@bouvet.no>
2023-01-31 13:23:51 +0100

    Outline for decryption pipeline test

---
+++ Bastion.Tests/IntegrationTests/Core/Domain/Decryption/Pipelines/DecryptAndDeleteSecretTest.cs
@@ -0,0 +1,18 @@
+using Bastion.Core.Domain.Encryption;
+using Bastion.Core.Domain.Encryption.Pipelines;
+using System.Security.Cryptography;
+
+namespace Bastion.Tests.Core.Domain.Decryption.Pipelines;
+
+public class DecryptAndDeleteSecretTest
+{
+    [Fact]
+    public void CreateUserSecret()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
