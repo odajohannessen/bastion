83211936bcfde11e09159dc6011e96a9c24a037a
odajoh <oda.johannessen@bouvet.no>
2023-02-04 13:42:02 +0100

    Delete default files, add about page and display secret page

---
+++ Bastion/Models/UserInputModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bastion.Models;
+
+public class UserInputModel : PageModel
+{
+    [Required]
+    //[StringLength(1, ErrorMessage = "Too long plaintext")] // TODO: Need an upper limit?
+    public string SecretPlaintext { get; set; } = "";
+
+    [Required]
+    public int Lifetime { get; set; } = 1;
+}
--- Bastion/Program.cs
+++ Bastion/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>(); // TODO: Remove
 builder.Services.AddTransient<IEncryptionService, EncryptionService>();
 builder.Services.AddTransient<IDecryptionService, DecryptionService>();
 builder.Services.AddTransient<IStorageService, StorageService>();
