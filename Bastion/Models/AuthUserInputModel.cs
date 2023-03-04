fa36180ed3c93d20e9bbcf9e351a16c33972e63e
odajoh <oda.johannessen@bouvet.no>
2023-03-04 16:05:45 +0100

    Create authenticated user input model, update form

---
+++ Bastion/Models/AuthUserInputModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bastion.Models;
+
+public class AuthUserInputModel : PageModel
+{
+    [Required]
+    [StringLength(5000, ErrorMessage = "Message is too long")]
+    public string SecretPlaintext { get; set; } = "";
+
+    [Required]
+    [Range(1, 24)]
+    public int Lifetime { get; set; } = 1;
+
+    [Required]
+    public string? OIDReceiver { get; set; }
+}
