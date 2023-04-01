7b4ccf20da7f343bcec9a45134a1b0d86271bc19
odajoh <oda.johannessen@bouvet.no>
2023-04-01 09:07:04 +0200

    Delete unused code and razor pages

--- Bastion/SharedKernel/BaseDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace Bastion.SharedKernel;
-
-public class BaseDomainEvent : INotification
-{
-    public DateTimeOffset DateOccurred { get; protected set; } = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
-}
